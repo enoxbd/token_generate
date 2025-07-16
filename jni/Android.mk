90570214d90eb07b88ad592ede3e5828e28799a9
enoxbd <enoxbd@gmail.com>
2025-07-17 03:04:01 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,7 +1,11 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE    := securetoken
 LOCAL_SRC_FILES := securetoken.cpp
-LOCAL_LDLIBS    := -llog -lcrypto
+
+# শুধু log লাইব দিয়ে রাখি
+LOCAL_LDLIBS    := -llog
+
 include $(BUILD_SHARED_LIBRARY)
