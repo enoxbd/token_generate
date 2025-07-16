f248c4e07727532e106d51cb35f2ac9934a035eb
enoxbd <enoxbd@gmail.com>
2025-07-17 05:53:36 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := secure_native
-LOCAL_SRC_FILES := main.cpp token_core.cpp sequrty_core.cpp
+LOCAL_SRC_FILES := main.cpp security_core.cpp token_core.cpp
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
