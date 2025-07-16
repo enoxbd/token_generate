da05e0dfa549920fd9154455fe7c69eb1770cd75
enoxbd <enoxbd@gmail.com>
2025-07-17 05:39:41 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := secure_native
 LOCAL_SRC_FILES := main.cpp token_core.cpp sequrty_core.cpp
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)  # header ফাইলের জন্য
+LOCAL_C_INCLUDES := $(LOCAL_PATH)
 
-LOCAL_LDLIBS := -llog -lcrypto
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
