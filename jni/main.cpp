512d2b7b500960a050029e84b85baec15c50af0c
enoxbd <enoxbd@gmail.com>
2025-07-17 14:57:36 +0600

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,5 +1,9 @@
 #include <jni.h>
+#include "token_core.hpp"  // generateSecureToken এর ডিক্লারেশন আছে
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv* env, jobject thiz, jobject context);
+Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv* env, jobject thiz, jobject context) {
+    std::string token = generateSecureToken(env, context);
+    return env->NewStringUTF(token.c_str());
+}
