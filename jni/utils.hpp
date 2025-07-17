f07b7ead34ae923378c262c8258998528e6de282
enoxbd <enoxbd@gmail.com>
2025-07-17 17:41:32 +0600

    Update utils.hpp

--- jni/utils.hpp
+++ jni/utils.hpp
@@ -1,8 +1,22 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <jni.h>
 #include <string>
 
-std::string getprop(const char* key);
+// SharedPreferences থেকে session_id নেবে
+std::string getSharedPreference(JNIEnv *env, jobject context, const std::string &key);
+
+// ডিভাইসের Android ID নিবে
+std::string getDeviceId(JNIEnv *env, jobject context);
+
+// ডিভাইসের fingerprint নিবে
+std::string getDeviceFingerprint(JNIEnv *env);
+
+// বর্তমান সময় স্ট্রিং আকারে নিবে
+std::string getCurrentTime();
+
+// দৈর্ঘ্য অনুযায়ী র‍্যান্ডম স্ট্রিং তৈরি করবে
+std::string generateRandomString(int length);
 
 #endif
