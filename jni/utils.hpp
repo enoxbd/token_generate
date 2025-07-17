34caaf8244534c30bf89ee149eb026ac071c9e25
enoxbd <enoxbd@gmail.com>
2025-07-17 18:21:16 +0600

    Update utils.hpp

--- jni/utils.hpp
+++ jni/utils.hpp
@@ -4,13 +4,6 @@
 #include <jni.h>
 #include <string>
 
-std::string getSharedPreference(JNIEnv *env, jobject context, const std::string &key);
-std::string getDeviceId(JNIEnv *env, jobject context);
-std::string getDeviceFingerprint(JNIEnv *env);
-std::string getCurrentTime();
-std::string generateRandomString(int length);
+std::string getSharedPreference(JNIEnv* env, jobject context, const std::string& key);
 
-std::string sha256(const std::string &input);
-std::string aesEncrypt(const std::string &data, const std::string &password);
-
-#endif
+#endif // UTILS_HPP
