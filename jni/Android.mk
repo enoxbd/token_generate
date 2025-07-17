d1a1f7203be6b6756aa3ab16d2660a2d3f599417
enoxbd <enoxbd@gmail.com>
2025-07-17 06:47:33 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -5,10 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := secure_native
 LOCAL_SRC_FILES := \
     token_core.cpp \
-    sha256_small.cpp \
     security_core.cpp \
-    main.cpp \
-    utils.cpp
+    utils.cpp \
+    main.cpp
 
 LOCAL_CPPFLAGS += -std=c++11
 
