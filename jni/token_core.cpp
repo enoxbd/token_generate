ac59a9b7d8715361569ae284207061d431f56ee9
enoxbd <enoxbd@gmail.com>
2025-07-17 17:35:24 +0600

    Update token_core.cpp

--- jni/token_core.cpp
+++ jni/token_core.cpp
@@ -1,78 +1,22 @@
 #include "token_core.hpp"
-#include <android/log.h>
-#include <string>
-#include <time.h>
-#include <cstdlib>
 #include "utils.hpp"
-#include "sha256_small.hpp"  // তোমার sha256 ফাংশন
+#include "sha256_small.hpp"
+#include "aes.hpp"
 
-#define LOG_TAG "TokenCore"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+std::string generateSecureToken(const std::string& session_id) {
+    std::string deviceId = getDeviceId();
+    std::string fingerprint = getDeviceFingerprint();
+    std::string currentTime = getCurrentTime();
+    std::string randomStr = generateRandomString(16);
 
-// SharedPreferences থেকে ডাটা পড়ার ফাংশন (তোমার কোড অনুসারে)
-std::string getSP(JNIEnv* env, jobject ctx, const char* name, const char* key) {
-    if (!env || !ctx) return "0";
+    // ডাটা গুলো একসাথে concatenate করলাম
+    std::string rawToken = session_id + "|" + deviceId + "|" + currentTime + "|" + fingerprint + "|" + randomStr;
 
-    jclass ctxCls = env->GetObjectClass(ctx);
-    if (!ctxCls) return "0";
+    // SHA256 হ্যাশ
+    std::string hashedToken = sha256(rawToken);
 
-    jmethodID getSP = env->GetMethodID(ctxCls, "getSharedPreferences", "(Ljava/lang/String;I)Landroid/content/SharedPreferences;");
-    if (!getSP) return "0";
+    // AES দিয়ে এনক্রিপ্ট
+    std::string encryptedToken = aesEncrypt(hashedToken, "enoxbdmontasir12");
 
-    jstring nm = env->NewStringUTF(name);
-    jobject sp = env->CallObjectMethod(ctx, getSP, nm, 0);
-    env->DeleteLocalRef(nm);
-
-    if (!sp) return "0";
-
-    jclass spCls = env->GetObjectClass(sp);
-    if (!spCls) return "0";
-
-    jmethodID getString = env->GetMethodID(spCls, "getString", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
-    if (!getString) return "0";
-
-    jstring jk = env->NewStringUTF(key);
-    jstring jdef = env->NewStringUTF("0");
-
-    jstring jval = (jstring)env->CallObjectMethod(sp, getString, jk, jdef);
-
-    env->DeleteLocalRef(jk);
-    env->DeleteLocalRef(jdef);
-
-    if (!jval) return "0";
-
-    const char* val = env->GetStringUTFChars(jval, 0);
-    std::string out(val);
-    env->ReleaseStringUTFChars(jval, val);
-    env->DeleteLocalRef(jval);
-
-    return out;
-}
-
-// Token generate করার মূল ফাংশন
-std::string generateSecureToken(JNIEnv* env, jobject ctx) {
-    std::string session = getSP(env, ctx, "User", "session_id");
-    std::string device = getprop("ro.serialno");
-    std::string fp = getprop("ro.build.fingerprint");
-
-    if (session == "0") session = "unknown";
-    if (device.empty()) device = "nodevice";
-    if (fp.empty()) fp = "nofingerprint";
-
-    long t = time(NULL);
-
-    static bool seeded = false;
-    if (!seeded) {
-        srand(t);
-        seeded = true;
-    }
-
-    int randv = rand() % 900000 + 100000;
-
-    std::ostringstream ss;
-    ss << session << ":" << device << ":" << fp << ":" << randv << ":" << t;
-    std::string base = ss.str();
-    LOGI("Base string: %s", base.c_str());
-
-    return sha256(base);
+    return encryptedToken;
 }
