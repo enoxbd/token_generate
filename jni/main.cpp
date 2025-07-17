3121da78f213cac437ee9a629adedf7f0334ebdf
enoxbd <enoxbd@gmail.com>
2025-07-18 04:06:40 +0600

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,31 +1,28 @@
 #include <jni.h>
 #include <string>
-#include <android/log.h>
-#include "TokenCore.hpp"
+#include "token_core.hpp"
 
-#define LOG_TAG "TokenGen"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+extern "C"
+JNIEXPORT jstring JNICALL
+Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv *env, jobject thiz, jobject context) {
+    // Call Java to get SharedPreferences values
+    jclass contextClass = env->GetObjectClass(context);
+    jmethodID getSharedPreferences = env->GetMethodID(contextClass, "getSharedPreferences", "(Ljava/lang/String;I)Landroid/content/SharedPreferences;");
+    jobject pref = env->CallObjectMethod(context, getSharedPreferences, env->NewStringUTF("User"), 0);
 
-std::string getSharedPrefString(JNIEnv* env, jobject context, const std::string& key) {
-    jclass ctxClass = env->GetObjectClass(context);
-    jmethodID getSharedPref = env->GetMethodID(ctxClass, "getSharedPreferences", "(Ljava/lang/String;I)Landroid/content/SharedPreferences;");
-    jobject sharedPref = env->CallObjectMethod(context, getSharedPref, env->NewStringUTF("User"), 0);
-
-    jclass prefClass = env->GetObjectClass(sharedPref);
+    jclass prefClass = env->GetObjectClass(pref);
     jmethodID getString = env->GetMethodID(prefClass, "getString", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
-    jstring value = (jstring) env->CallObjectMethod(sharedPref, getString, env->NewStringUTF(key.c_str()), env->NewStringUTF(""));
-    
-    const char* chars = env->GetStringUTFChars(value, nullptr);
-    std::string result(chars);
-    env->ReleaseStringUTFChars(value, chars);
-    return result;
-}
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv* env, jclass clazz, jobject context) {
-    std::string sessionId = getSharedPrefString(env, context, "User");
-    std::string deviceId = getSharedPrefString(env, context, "device_id");
+    jstring jSession = (jstring) env->CallObjectMethod(pref, getString, env->NewStringUTF("session_id"), env->NewStringUTF(""));
+    jstring jDevice = (jstring) env->CallObjectMethod(pref, getString, env->NewStringUTF("device_id"), env->NewStringUTF(""));
 
-    std::string token = generateSecureToken(sessionId, deviceId);
+    const char* session = env->GetStringUTFChars(jSession, 0);
+    const char* device = env->GetStringUTFChars(jDevice, 0);
+
+    std::string token = generateSecureToken(session, device);
+
+    env->ReleaseStringUTFChars(jSession, session);
+    env->ReleaseStringUTFChars(jDevice, device);
+
     return env->NewStringUTF(token.c_str());
 }
