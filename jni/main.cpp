86c56d0b69d97135d58f112d851da63a888033c5
enoxbd <enoxbd@gmail.com>
2025-07-17 07:02:27 +0600

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -2,20 +2,22 @@
 #include <android/log.h>
 #include <stdlib.h>
 
-#include "security_core.hpp"  // header ফাইল include করো
-#include "token_core.hpp"     // header ফাইল include করো
+#include "security_core.hpp"
+#include "token_core.hpp"
 #include "utils.hpp"
 
 #define LOG_TAG "MainSecure"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
-// Application package name অনুযায়ী SO ফাইলের path
 #define SO_PATH "/data/data/com.my.newproject8/lib/libsecure_native.so"
+
+// detectThreats এর extern "C" প্রোটোটাইপ
+extern "C" bool detectThreats(JNIEnv* env, jobject ctx);
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv* env, jobject thiz, jobject context) {
-    // ১) SO file exists check
+    // SO ফাইল আছে কিনা চেক
     FILE* f = fopen(SO_PATH, "r");
     if (!f) {
         LOGI("SO file missing! Exiting...");
@@ -23,13 +25,13 @@ Java_com_my_newproject8_SecureManager_getSecureToken(JNIEnv* env, jobject thiz, 
     }
     fclose(f);
 
-    // ২) Core security checks
+    // থ্রেট ডিটেকশন
     if (detectThreats(env, context)) {
         LOGI("Threat found! Exiting...");
         exit(0);
     }
 
-    // ৩) Valid shortcut token generation
+    // সিকিউর টোকেন জেনারেশন
     std::string token = generateSecureToken(env, context);
     return env->NewStringUTF(token.c_str());
 }
