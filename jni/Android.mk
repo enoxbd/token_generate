83090db7b4297cf4d47d976bac189e6a1bc4add2
enoxbd <enoxbd@gmail.com>
2025-07-17 18:35:08 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -10,9 +10,10 @@ LOCAL_SRC_FILES := \
     token_core.cpp \
     utils.cpp \
     sha256_small.cpp \
-    aes_core.cpp
+    aes.cpp   # এখানেই aes_core.cpp এর জায়গায় aes.cpp দিবে
 
 LOCAL_CPPFLAGS += -std=c++11
+
 LOCAL_LDLIBS := -llog -landroid
 
 include $(BUILD_SHARED_LIBRARY)
