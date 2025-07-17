04abbc9d57d5488a24c02eaa694d2b8bb5a63dae
enoxbd <enoxbd@gmail.com>
2025-07-17 17:34:43 +0600

    Update token_core.hpp

--- jni/token_core.hpp
+++ jni/token_core.hpp
@@ -1,9 +1,9 @@
 #ifndef TOKEN_CORE_HPP
 #define TOKEN_CORE_HPP
 
-#include <jni.h>
 #include <string>
 
-std::string generateSecureToken(JNIEnv* env, jobject ctx);
+// এই ফাংশন টোকেন তৈরি করবে, session_id ইনপুট নিবে
+std::string generateSecureToken(const std::string& session_id);
 
 #endif
