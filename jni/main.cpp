3953e5c23a9b8aa3390a0c88ab4a0f7dab88f6ac
enoxbd <enoxbd@gmail.com>
2025-07-17 19:33:15 +0600

    Update main.cpp

--- jni/main.cpp
+++ jni/main.cpp
@@ -8,6 +8,7 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
+// JNI Function: Java theke call hobe
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_my_newproject8_SecureManager_getSecureToken(
@@ -15,6 +16,7 @@ Java_com_my_newproject8_SecureManager_getSecureToken(
         jobject /* this */,
         jobject context) {
 
+    // Call the token generator with SharedPreferences key "User"
     std::string token = tokencore::generateSecureToken(env, context, "User");
     LOGI("Generated Token: %s", token.c_str());
     return env->NewStringUTF(token.c_str());
