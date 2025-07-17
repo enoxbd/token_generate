148a215d6c8a8cdae7aad23b3e1f920f70cd6a60
enoxbd <enoxbd@gmail.com>
2025-07-17 18:00:44 +0600

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,13 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := enoxbd
+LOCAL_MODULE    := enoxbd
 LOCAL_SRC_FILES := \
     main.cpp \
+    security_core.cpp \
     token_core.cpp \
     utils.cpp
 
 LOCAL_CPPFLAGS += -std=c++11
-LOCAL_LDLIBS := -llog
+LOCAL_LDLIBS   := -llog
 
 include $(BUILD_SHARED_LIBRARY)
